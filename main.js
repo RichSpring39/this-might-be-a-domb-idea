b811aa04189638aa8f317dfbaac671d914a1b665
RichSpring39 <richard.springer@codeimmersives.com>
2022-10-27 08:55:14 -0600

    update

---
+++ main.js
@@ -0,0 +1,30 @@
+// Change the font color of the paragraph to light blue.
+
+let para = document.querySelector("p");
+
+para.style.color = "lightblue";
+
+//Change the font size of the heading to 40px.
+let head = document.querySelector("h1");
+head.style.fontSize = "40px";
+
+para.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.";
+
+let item13 = document.querySelector("#item-13");
+item13.style.opacity = "0.5";
+
+let item3 = document.querySelector("#item-3");
+item3.innerText = "I say, Hi!";
+
+let item16 = document.querySelector("#item-16");
+item16.innerText = "Won't get fooled again.";
+
+let myImage = document.querySelector("img");
+myImage.src = "https://cdna.artstation.com/p/assets/images/images/043/478/098/large/dominick-critelli-deepspace-11192021.jpg?1637368880";
+
+myImage.style.height = `300px`
+
+let myImage1 = document.querySelector("#Ig");
+myImage1.src = "https://www.domusweb.it/content/domusweb20/en/architecture/gallery/2020/12/21/night-city-com-stata-costruita-la-citt-mondo-di-cyberpunk-2077/jcr:content/image-preview.img.rmedium.jpg/1608713300753.jpg";
+
+myImage1.style.height = `300px`
